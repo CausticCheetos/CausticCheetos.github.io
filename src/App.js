3644f8db98c290ff20b1beb3551c3f2da99369f0
Andy Le <andy.le314@gmail.com>
2023-07-16 19:32:24 +1000

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import Skills from './components/Skills'
+import Projects from './components/Projects'
+import Contact from './components/Contact'
+import Experience from './components/Experience'
+import AppBar from '@mui/material/AppBar'
+import Button from '@mui/material/Button'
+import { useEffect, useState, useRef } from 'react'
 
 function App() {
+  
+  const time = new Date()
+  const hour = time.getHours()
+ 
+  const handleClick = (id) => {
+    console.log(id);
+    const element = document.getElementById(id)
+    element.scrollIntoView({behavior: "smooth"})
+  }
+
   return (
     <div className="App">
+
+      <AppBar position="sticky" style={{backgroundColor:"transparent"}}>
+        <div className='navContainer'>
+        <Button color="inherit" onClick={() => handleClick('about')}>About</Button>
+        <Button color="inherit" onClick={() => handleClick('experience')}>Experience</Button>
+        <Button color="inherit" onClick={() => handleClick('projects')}>Projects</Button>
+        <Button color="inherit" onClick={() => handleClick('contact')}>Contact</Button>
+        </div>
+      </AppBar>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {hour < 12 ? "Good morning" : (hour < 18 ? "Good afternoon" : "Good evening")}, my name is Andy Le.
       </header>
+        <div
+          id='about'
+          className="page">
+          <p>I'm a Software Engineering student at the University of Technology Sydney.</p>
+          <p> I have an interest in web development and am in particular working towards becoming a full stack developer.</p>
+
+          <h1>Technical Skills </h1>
+          
+          <div>
+            <Skills/>
+          </div>
+        </div>
+
+        <Experience/>
+
+        <Projects/>
+
+        <Contact/>
     </div>
   );
 }
---
+++ src/components/Experience.js
@@ -0,0 +1,21 @@
+
+
+const Experience = () => {
+
+    return (
+        <div id='experience' className="page">
+            <h1>Work Experience</h1>
+            <div className="card">
+                <h2>8Common</h2>
+                <h3>Java Developer Intern</h3>
+                <h3>March 2022 - November 2022</h3>
+                <p>Created unit test to increase code base coverage and performed library upgrades.</p>
+                <p>Gained exposure to J2EE, Junit5, Mockito, and scrum methodology.</p>
+            </div>
+        </div>
+    )
+
+
+}
+
+export default Experience
